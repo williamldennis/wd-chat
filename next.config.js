ec1cb612b6257d55cc9be8b70851d2d40ecd7cbc
Will Dennis <williamldennis@gmail.com>
2025-06-13 09:41:12 -0400

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
+// next.config.js (with ESM)
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
-module.exports = {
+import './src/env.js';
+
+/** @type {import('next').NextConfig} */
+const config = {
     experimental: {
         optimizeCss: false, // disables LightningCSS for CSS
     },
-}
-
-/** @type {import("next").NextConfig} */
-const config = {};
+};
 
 export default config;
