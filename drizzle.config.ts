2d40d76184c837fdf66a934c6ad8c1c902877d4c
Will Dennis <williamldennis@gmail.com>
2025-06-17 09:44:37 -0400

    switched db to supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.NEXT_PUBLIC_SUPABASE_URL,
   },
   tablesFilter: ["wd-chat_*"],
 } satisfies Config;
