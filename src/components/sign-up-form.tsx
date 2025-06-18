16b35348fdf7ed3b6d17f559b98e04168104fcc5
Will Dennis <williamldennis@gmail.com>
2025-06-18 13:39:11 -0400

    matched login to sign up, added links between them

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -176,7 +176,7 @@ export function SignUpForm({
                                 </div>
                                 <div className="text-center text-sm">
                                     Already have an account?{" "}
-                                    <a href="#" className="underline underline-offset-4">
+                                    <a href="/login" className="underline underline-offset-4">
                                         Log In
                                     </a>
                                 </div>
